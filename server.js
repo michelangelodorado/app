ae0868c31ee8230fdfb916a4019cc06cfeb9f497
Mike <michelangelodorado@gmail.com>
2023-03-15 21:01:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ const router = express.Router();
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
-// App
 const app = express();
+// default path
 app.get('/', (req, res) => {
   const srcipAddress = req.socket.remoteAddress;
   const dstipAddress = req.socket.localAddress;
   res.send(`<p>Date: ${new Date()}</p><p>Source Address:  <b>${srcipAddress}</b></p><p>Destination Address:  <b>${dstipAddress}</b></p><p>append <b>/header</b> if you want to see request headers</p>`);
 });
-
+// header path
 app.get('/header', function (req, res) {
  res.json(req.headers);
 });
