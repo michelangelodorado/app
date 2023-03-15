99c7fae819734e09a3b24a847c9e5553e83a5e64
Mike <102953584+michelangelodorado@users.noreply.github.com>
2023-03-15 20:29:29 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+
+// the router
+const router = express.Router();
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  const srcipAddress = req.socket.remoteAddress;
+  const dstipAddress = req.socket.localAddress;
+  res.send(`<p>Date: ${new Date()}</p><p>Source Address:  <b>${srcipAddress}</b></p><p>Destination Address:  <b>${dstipAddress}</b></p><p>append <b>/header</b> if you want to see request headers</p>`);
+});
+
+app.get('/header', function (req, res) {
+ res.json(req.headers);
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
